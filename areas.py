b41e349a76efd260299ad6f2184d1df1b40a24aa
Tatiana-tatvch <78843996+Tatiana-tatvch@users.noreply.github.com>
2021-02-10 11:26:38 +0300

    Add files via upload

---
+++ areas.py
@@ -0,0 +1,24 @@
+# Площадь треугольника
+base = 4 
+height = 5
+area = (base * height) / 2
+print("Площадь треугольника", area)
+
+# Площадь квадрата
+side_a = 4
+area = side_a ** 2
+print("Площадь квадрата", area)
+
+# Площадь прямоугольника
+side_a = 4
+side_b = 5
+area = side_a * side_b
+print("Площадь прямоугольника", area)
+
+
+# Площадь круга
+pi = 3.14159
+diameter = 8
+radius = diameter / 2
+area = pi * radius ** 2
+print("Площадь круга", area)
